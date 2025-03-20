c4dc16f158118c212f2ab84fee48bc9e4fb1871e
Mateo Buela <120684119+MateoBD@users.noreply.github.com>
2025-03-20 09:03:10 -0300

    structures: change name variables

--- include/structures.h
+++ include/structures.h
@@ -1,23 +1,24 @@
 #ifndef STRUCTURES_H
 #define STRUCTURES_H
+#include <stdbool.h>
 #include <sys/wait.h>
 #include <semaphore.h>
 typedef struct { 
-    char namePlayer[16]; // Nombre del jugador 
+    char name_player[16]; // Nombre del jugador 
     unsigned int points; // Puntaje 
-    unsigned int cantInvalidMovements; // Cantidad de solicitudes de movimientos inválidas realizadas 
-    unsigned int cantValidMovements; // Cantidad de solicitudes de movimientos válidas realizadas 
+    unsigned int cant_invalid_movements; // Cantidad de solicitudes de movimientos inválidas realizadas 
+    unsigned int cant_valid_movements; // Cantidad de solicitudes de movimientos válidas realizadas 
     unsigned short x, y; // Coordenadas x e y en el tablero 
     pid_t pid; // Identificador de proceso 
-    bool canMove; // Indica si el jugador tiene movimientos válidos disponibles 
+    bool can_move; // Indica si el jugador tiene movimientos válidos disponibles 
   } playerStatus;
 
 typedef struct { 
   unsigned short width; // Ancho del tablero 
   unsigned short heigth; // Alto del tablero 
-  unsigned int cantPlayers; // Cantidad de jugadores 
+  unsigned int cant_players; // Cantidad de jugadores 
   playerStatus players[9]; // Lista de jugadores  
-  bool canEnd; // Indica si el juego se ha terminado 
+  bool can_end; // Indica si el juego se ha terminado 
   int board[]; // Puntero al comienzo del tablero. fila-0, fila-1, ..., fila-n-1  
 } gameStatus;
 
