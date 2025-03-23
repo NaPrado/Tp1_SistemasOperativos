7066a42fbdf7e37f76af5a7f4988cbb4a3c64dc2
Mateo Buela <120684119+MateoBD@users.noreply.github.com>
2025-03-22 21:25:00 -0300

    "fixed" semaphores (and added comments)

--- code/player.c
+++ code/player.c
@@ -28,28 +28,28 @@ int main(int argc, char const *argv[]){
     }
     
     while (!game_state->players[player].can_move){
-        //entry section
-        sem_wait(master_mutex);
-        sem_wait(player_read_count_mutex);
-        game_sync->player_reading_status++; 
-        if (game_sync->player_reading_status == 1) {
-            sem_wait(game_state_mutex);
+        // entry section
+        sem_wait(game_state_mutex);     // espero a entrar
+        sem_wait(player_read_count_mutex);      // espero modificar variable
+        game_sync->player_reading_status++;     // modifico variable
+        if (game_sync->player_reading_status == 1) {    // si soy el primero
+            sem_wait(master_mutex);                // espero a que no haya writer
         }
-        sem_post(player_read_count_mutex);
-        sem_post(master_mutex);
+        sem_post(player_read_count_mutex);      // dejo modificar variable
+        sem_post(game_state_mutex);     // dejo entrar
 
         //critical zone
         
-        
-       
-       //exit Section
-       sem_wait(player_read_count_mutex);
-       game_sync->player_reading_status--;
-       if (game_sync->player_reading_status == 0) {
-           sem_post(game_state_mutex);
+
+        //exit Section
+        sem_wait(player_read_count_mutex);      // espero modificar variable
+        game_sync->player_reading_status--;     // modifico variable
+        if (game_sync->player_reading_status == 0) {    // si soy el ultimo
+            sem_post(master_mutex);                // dejo que haya writer
         }
-        sem_post(player_read_count_mutex); 
-        putchar(randInt(0,7));
+        sem_post(player_read_count_mutex);      // dejo modificar variable
+        putchar(randInt(0,7));  
+        
     }
     return 0;
 }
