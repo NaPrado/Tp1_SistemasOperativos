adc4cdf237efc0c29a6044267ab6293a01a1c4f9
Nahuel Prado <141145234+NaPrado@users.noreply.github.com>
2025-03-23 18:17:49 -0300

    hago lo que puedo loco

--- code/player.c
+++ code/player.c
@@ -6,6 +6,20 @@
 
 #include <fcntl.h>
 
+can_player_move(int* board,int width,int height,int x, int y){
+    bool ret=false;
+    for (size_t i = -1; i < 2; i++){
+        for (size_t j = -1; j < 2; j++){
+            if (i!=0 && j!=0 && x+i<width && y+j<height){
+                ret=(board[width*(x+i)+y+j]>0);
+                if (ret){
+                    return ret;
+                }
+            }
+        }
+    }
+    
+}
 
 int main(int argc, char const *argv[]){
 
@@ -21,32 +35,25 @@ int main(int argc, char const *argv[]){
     sem_t * game_state_mutex= &(game_sync->game_state_mutex);
     sem_t * master_mutex= &(game_sync->master_mutex);
     sem_t * player_read_count_mutex= &(game_sync->player_read_count_mutex);
-    int player;
+    int player_number;
     pid_t pid=getpid();
     for (size_t i = 0; i < game_state->cant_players; i++){
         if ((game_state->players[i].pid)==pid){
-            player=i;  
+            player_number=i;  
         }
     }
 
-    int file = open("debug.txt", O_CREAT | O_RDWR | O_TRUNC);
+    FILE * file = fopen("debug.txt", "w+");
 
+    bool cant_move=false;
     
-    while (!game_state->players[player].can_move){
-
-        // char aux = game_state->players[player].can_move;
-        
-        char buf[] = {'0'};
-        write(file, buf, 1);
-
+    while (!cant_move){
         
         putchar(randInt(0,7));
 
-
-        // entry section
+         // entry section
 
         sem_wait(master_mutex);
-
         sem_wait(player_read_count_mutex);      // espero modificar variable
         game_sync->player_reading_status++;     // modifico variable
 
@@ -58,7 +65,7 @@ int main(int argc, char const *argv[]){
 
         //critical zone
 
-
+        cant_move=can_player_move(game_state->board,width,height,game_state->players[player_number].x,game_state->players[player_number].y);
 
         //exit Section
         sem_wait(player_read_count_mutex);      // espero modificar variable
@@ -66,12 +73,12 @@ int main(int argc, char const *argv[]){
         if (game_sync->player_reading_status == 0) {    // si soy el ultimo
             sem_post(game_state_mutex);                // dejo que haya writer
         }
-        sem_post(player_read_count_mutex);      // dejo modificar variable
+        sem_post(player_read_count_mutex);      // dejo modificar variable 
 
         
     }
 
-    close(file);
+    fclose(file);
 
     return 0;
 }
