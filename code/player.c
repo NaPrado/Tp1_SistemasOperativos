6492c4fd0a4f1e226e63e6f6c7e8b644cd203d85
Nahuel Prado <141145234+NaPrado@users.noreply.github.com>
2025-03-22 21:02:55 -0300

    view fancy

--- code/player.c
+++ code/player.c
@@ -15,34 +15,41 @@ int main(int argc, char const *argv[]){
     //chequear el size
     game_status * game_state = get_game_state(sizeof(game_status) + (sizeof(int) * (height * width)));
     randomize();
-    /*bool can_end=false;
+    bool can_end=false;
     sem_t * game_state_mutex= &(game_sync->game_state_mutex);
     sem_t * master_mutex= &(game_sync->master_mutex);
-    sem_t * player_read_count_mutex= &(game_sync->player_read_count_mutex); */
-    while (!game_state->can_end){
-        /* //entry section
+    sem_t * player_read_count_mutex= &(game_sync->player_read_count_mutex);
+    int player;
+    pid_t pid=getpid();
+    for (size_t i = 0; i < game_state->cant_players; i++){
+        if ((game_state->players[i].pid)==pid){
+            player=i;  
+        }
+    }
+    
+    while (!game_state->players[player].can_move){
+        //entry section
         sem_wait(master_mutex);
         sem_wait(player_read_count_mutex);
         game_sync->player_reading_status++; 
         if (game_sync->player_reading_status == 1) {
-            sem_wait(&game_state_mutex);
+            sem_wait(game_state_mutex);
         }
         sem_post(player_read_count_mutex);
         sem_post(master_mutex);
 
         //critical zone
         
-
-        //exit Section
-        sem_wait(player_read_count_mutex);
-        game_sync->player_reading_status--;
-        if (game_sync->player_reading_status == 0) {
-            sem_post(&game_state_mutex);
+        
+       
+       //exit Section
+       sem_wait(player_read_count_mutex);
+       game_sync->player_reading_status--;
+       if (game_sync->player_reading_status == 0) {
+           sem_post(game_state_mutex);
         }
-        sem_post(player_read_count_mutex);  */
+        sem_post(player_read_count_mutex); 
         putchar(randInt(0,7));
-        //sem_post(&master_mutex);
     }
-
     return 0;
 }
--- code/view.c
+++ code/view.c
@@ -8,29 +8,50 @@
 
 // Vector de códigos de color
 const char *colores[] = {
-    "\033[0;30m",  // Negro
-    "\033[0;31m",  // Rojo
-    "\033[0;32m",  // Verde
-    "\033[0;33m",  // Amarillo
-    "\033[0;34m",  // Azul
-    "\033[0;35m",  // Magenta
-    "\033[0;36m",  // Cian
-    "\033[0;37m",  // Blanco
-    "\033[0;90m"   // Gris claro
+    "\033[40;97m",  // Fondo negro, letra blanca
+    "\033[41;97m",  // Fondo rojo, letra blanca
+    "\033[42;30m",  // Fondo verde, letra negra
+    "\033[43;30m",  // Fondo amarillo, letra negra
+    "\033[44;97m",  // Fondo azul, letra blanca
+    "\033[45;97m",  // Fondo magenta, letra blanca
+    "\033[46;30m",  // Fondo cian, letra negra
+    "\033[47;30m",  // Fondo blanco, letra negra
+    "\033[100;97m"  // Fondo gris claro, letra blanca
+};
+const char *colorescabeza[] = {
+    "\033[48;5;235;97m",  // Cabeza con fondo gris más oscuro y letra blanca
+    "\033[48;5;196;97m",  // Cabeza con fondo rojo y letra blanca
+    "\033[48;5;28;97m",   // Cabeza con fondo verde oscuro y letra blanca
+    "\033[48;5;130;97m",  // Cabeza con fondo marrón oscuro y letra blanca
+    "\033[48;5;32;97m",   // Cabeza con fondo azul y letra blanca
+    "\033[48;5;125;97m",  // Cabeza con fondo magenta oscuro y letra blanca
+    "\033[48;5;51;97m",   // Cabeza con fondo cian oscuro y letra blanca
+    "\033[48;5;15;97m",   // Cabeza con fondo blanco y letra negra
+    "\033[48;5;237;97m"   // Cabeza con fondo gris oscuro y letra blanca
 };
 
-const char *colores_reset = "\033[0m"; // Restablecer colores
+
+const char *colores_reset = "\033[0;0m"; // Restablecer colores
 
 void print_view(int * board, size_t height, size_t width, game_status* game_state) {
-    for (size_t i = 0; i < width; i++){
-        for (size_t j = 0; j < height; j++){
+    for (size_t i = 0; i < height; i++){
+        for (size_t j = 0; j < width; j++){
             for (size_t k = 0; k < game_state->cant_players; k++){
-                 if(game_state->players[k].x==i && game_state->players[k].y==j){
-                    printf("%s",colores[k]);
+                if(board[j+i*width]==-k){
+                    if (game_state->players[k].x==j &&game_state->players[k].y==i){
+                        printf("%s",colorescabeza[k]);
+                    }
+                    else{
+                        printf("%s",colores[k]);
+                    }
                 } 
             }
-            printf("%d,%s",board[i+j*width],colores_reset);
-
+            if (board[j+i*width]<0){
+                printf("|%d |%s",board[j+i*width],colores_reset);
+            }
+            else{
+                printf("| %d |%s",board[j+i*width],colores_reset);
+            }
         }
         printf("\n");
     }
