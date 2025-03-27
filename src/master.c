cddafe2f96d61eb98281bc33c147d6ec84a8ca45
Mateo Buela <120684119+MateoBD@users.noreply.github.com>
2025-03-26 21:13:50 -0300

    master autentification parameters (not working yet)

--- src/master.c
+++ src/master.c
@@ -1,5 +1,88 @@
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <stdio.h>
 
-int main(int argc, char const *argv[]){
+enum params_default {DEF_WIDTH = 10, DEF_HEIGHT = 10, DEF_DELAY = 200, DEF_TIMEOUT = 10};
+
+#define FLAG_WIDTH "-w"
+#define FLAG_HEIGHT "-h"
+#define FLAG_DELAY "-d"
+#define FLAG_SEED "-s"
+#define FLAG_VIEW "-v"
+#define FLAG_TIMEOUT "-t"
+#define FLAG_PLAYER "-p"
+
+#define MAX_NUM_PLAYERS 9
+
+int setParams(int argc, char const *argv[], int * num_params, char ** view, char * players[MAX_NUM_PLAYERS]);
+
+int main(int argc, char const *argv[]) {
+
+    int num_params[5] = {0};
+    char * view = NULL;
+    char * players[MAX_NUM_PLAYERS] = {NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL, NULL};
+
+    setParams(argc, argv, num_params, &view, players);
+    
+    printf("Width: %d\n", num_params[0]);
+    printf("Height: %d\n", num_params[1]);
+    printf("Delay: %d\n", num_params[2]);
+    printf("Seed: %d\n", num_params[3]);
+    printf("Timeout: %d\n", num_params[4]);
+    printf("View: %s\n", view);
+    for (int i = 0; i < MAX_NUM_PLAYERS; i++) {
+        if (players[i] != NULL) {
+            printf("Player %d: %s\n", i, players[i]);
+        }
+    }
+    
     
     return 0;
+}
+
+int setParams(int argc, char const *argv[], int * num_params, char ** view, char * players[MAX_NUM_PLAYERS]) {
+    int i = 1;
+    while (i+1 < argc) {
+        if (strcmp(argv[i], FLAG_WIDTH) == 0) {
+            if ((num_params[0] = atoi(argv[i+1])) == 0) {
+                num_params[0] = DEF_WIDTH;
+            }
+            i += 2;
+        } else if (strcmp(argv[i], FLAG_HEIGHT) == 0) {
+            if ((num_params[1] = atoi(argv[i+1])) == 0) {
+                num_params[1] = DEF_HEIGHT;
+            }
+            i += 2;
+        } else if (strcmp(argv[i], FLAG_DELAY) == 0) {
+            if ((num_params[2] = atoi(argv[i+1])) == 0) {
+                num_params[2] = DEF_DELAY;
+            }
+            i += 2;
+        } else if (strcmp(argv[i], FLAG_SEED) == 0) {
+            if ((num_params[3] = atoi(argv[i+1])) == 0) {
+                num_params[3] = time(NULL);
+            }
+            i += 2;
+        } else if (strcmp(argv[i], FLAG_VIEW) == 0) {
+            *view = argv[i+1];
+            i += 2;
+        } else if (strcmp(argv[i], FLAG_TIMEOUT) == 0) {
+            if ((num_params[5] = atoi(argv[i+1])) == 0) {
+                num_params[5] = DEF_TIMEOUT;
+            }
+            i += 2;
+        } else if (strcmp(argv[i], FLAG_PLAYER) == 0) {
+            int j = 0;
+            while (players[j] != NULL) {
+                j++;
+            }
+            players[j] = argv[i+1];
+            i += 2;
+        } else {
+            return 1;
+        }
+    }
+    return 0;
+    
 }
